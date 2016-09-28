5f61ccd91a2c25cea5b18d8cdd0939706c32713b
vgairola <vgairola@github.com>
2016-09-28 21:42:36 +0530

    insert scripts

---
+++ DB Scripts/ccm_insert_script.sql
@@ -0,0 +1,5 @@
+-- role table insert script
+insert into role values (1, 'ROLE_ADMIN');
+
+-- user table insert script
+INSERT INTO `user` (`userId`,`userName`,`password`,`enabled`,`displayName`,`roleId`) VALUES (1,'vijay@gmail.com','f375f401ddc698af533f16f8ac1e91c1',1,'Vijay',1);
